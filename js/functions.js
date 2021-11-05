0d04b05d545074d006d5f73a859ab7bca0c25b88
Ouderbijslag <KerimOfficieel@gmail.com>
2021-11-05 04:42:10 +0100

    innerHTML opdracht

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+document.getElementById("p1")
+
+document.getElementById("p2")
+
+document.querySelector(".p3")
+
+document.querySelector(".p4")
+
+document.getElementById("logo")
+
+document.querySelector("body").innerHTML = "alles weg";
